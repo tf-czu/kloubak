ba05fb1f8b194659d5b6039c9554e06f7a830819
tajgr <levjakub@gmail.com>
2022-11-01 20:33:51 +0100

    app/run_jetson.py: refactor to osgar node

--- app/run_jetson.py
+++ app/run_jetson.py
@@ -7,10 +7,10 @@ import zmq
 import time
 import osgar.lib.serialize
 import sys
+from threading import Thread
 
 
-def push_msg(data, ip = "127.0.0.1"):
-    endpoint = f"tcp://{ip}:5550"
+def push_msg(msg, endpoint):
     context = zmq.Context.instance()
     socket = context.socket(zmq.PUSH)
 
@@ -21,20 +21,46 @@ def push_msg(data, ip = "127.0.0.1"):
 
     try:
         with contextlib.closing(socket):
-            raw = osgar.lib.serialize.serialize(data)
+            raw = osgar.lib.serialize.serialize(msg)
             socket.send_multipart([bytes("control_msg", 'ascii'), raw])
     except zmq.ZMQError as e:
         print(e)
 
+class RunJetson:
+    def __init__(self, config, bus):
+        self.input_thread = Thread(target=self.run_input, daemon=True)
+        self.bus = bus
+        bus.register('msg')
+
+        host = config.get('host', '127.0.0.1')
+        self.task = config.get('task')  # path to config file
+        assert self.task is not None
+        self.endpoint = f"tcp://{host}:5550"
+
+
+    def run_input(self):
+        self.push_msg(self.task, self.endpoint)
+        while self.bus.is_alive():
+            self.bus.sleep(2)
+
+    def start(self):
+        self.input_thread.start()
+
+    def join(self, timeout=None):
+        self.push_msg('quit', self.endpoint)
+        self.input_thread.join(timeout=timeout)
+
+    def request_stop(self):
+        self.bus.shutdown()
+
+
 if __name__ == "__main__":
-    if len(sys.argv) < 2:
+    if len(sys.argv) != 3:
         print(__doc__)
         sys.exit()
-    if len(sys.argv) == 3:
-        push_msg(sys.argv[1], sys.argv[2])  # config file and ip
-        time.sleep(10)
-        push_msg("quit", sys.argv[2])
-    else:
-        push_msg(sys.argv[1])
-        time.sleep(10)
-        push_msg("quit")
+
+    host = sys.argv[2]
+    endpoint = f"tcp://{host}:5550"
+    push_msg(sys.argv[1], endpoint)  # config file and ip
+    time.sleep(10)
+    push_msg("quit", endpoint)
