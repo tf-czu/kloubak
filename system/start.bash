769e83d7f5e4b965f5de4e638c1ca3ae65d3ad88
tajgr <levjakub@gmail.com>
2023-01-31 21:28:54 +0100

    system/start.bash: add sleep to screen

--- system/start.bash
+++ system/start.bash
@@ -7,8 +7,8 @@ ifup br0
 echo "Set date on Jetson nano units and run launchers"
 sleep 1
 ssh k2jetson_front -t "sudo date +%c -s '$(date +%c)'"
-ssh k2jetson_front -t "screen -dmL python3 git/kloubak/system/launcher_jetson.py"
+ssh k2jetson_front -t "screen -dmL python3 git/kloubak/system/launcher_jetson.py && sleep 1"
 ssh k2jetson_rear -t "sudo date +%c -s '$(date +%c)'"
-ssh k2jetson_rear -t "screen -dmL python3 git/kloubak/system/launcher_jetson.py"
+ssh k2jetson_rear -t "screen -dmL python3 git/kloubak/system/launcher_jetson.py && sleep 1"
 cd /home/robot/git/osgar
 screen
